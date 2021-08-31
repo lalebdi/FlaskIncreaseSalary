d13712d4bd4c4b3fa9658e8d460e628b6db58266
lalebdi <loai.alebdi@gmail.com>
2021-08-30 21:09:49 -0400

    increase the salary 5%

--- application.py
+++ application.py
@@ -6,17 +6,25 @@ app = application
 api = Api(app)
 
 put_args = reqparse.RequestParser()
-put_args.add_argument("name", type=str, help="Name of employee is required", required=True)
-put_args.add_argument("position", type=str, help="Position of employee")
-put_args.add_argument("salary", type=int, help="How much they make? it is required", required=True)
+put_args.add_argument("name", type=str, help="Name of employee is required")
+put_args.add_argument("salary", type=int, help="How much they make? it is required")
 
-employees = {"tony" : {"name" : "Tony Stark", "Position": "Ironman", "salary": 10000},
-            "bruce": {"name" : "Bruce Wayne", "Position": "Batman", "salary": 20000},
-            "jack": {"name" : "Jack Sparrow", "Position": "Pirate", "salary": 30000}}
+employees = {"tony" : {"name" : "Tony Stark", "salary": 10000},
+            "bruce": {"name" : "Bruce Wayne", "salary": 20000},
+            "jack": {"name" : "Jack Sparrow", "salary": 30000}}
 
 def abort_if_employees_not_exist(name):
     if name not in employees:
         abort(404, message="Employee name does not exist")
+
+
+def process_increase_salary(profile):
+    initial_salary = employees[profile]["salary"]
+    PERCENT_INCREASE = 1.05
+    post_increase = initial_salary * PERCENT_INCREASE
+    employees[profile]["salary"] = post_increase
+
+
 
 class IncreaseSalary(Resource):
     def get(self, name):
@@ -25,8 +33,7 @@ class IncreaseSalary(Resource):
 
     def put(self, name):
         args = put_args.parse_args()
-        employees[name] = args
-        # output_data = processing_data(args)
+        process_increase_salary(name)
         return employees[name], 201
 
 
