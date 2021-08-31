0167ff99487ca38118981ccdad8e4dd3caae484a
lalebdi <loai.alebdi@gmail.com>
2021-08-30 20:42:07 -0400

    get and put with sample data

--- application.py
+++ application.py
@@ -1,25 +1,36 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 application = Flask(__name__)
 app = application
 api = Api(app)
 
-post_args = reqparse.RequestParser()
-post_args.add_argument("value", type=str, help="Value is required to proceed", required=True)
-post_args.add_argument("mode", type=str, help="Choose between phone || name || amount", required=True)
-post_args.add_argument("replace_with", type=str, help="Choose either --blank-- || --original--", required=True)
+put_args = reqparse.RequestParser()
+put_args.add_argument("name", type=str, help="Name of employee is required", required=True)
+put_args.add_argument("position", type=str, help="Position of employee")
+put_args.add_argument("salary", type=int, help="How much they make? it is required", required=True)
 
+employees = {"tony" : {"name" : "Tony Stark", "Position": "Ironman", "salary": 10000},
+            "bruce": {"name" : "Bruce Wayne", "Position": "Batman", "salary": 20000},
+            "jack": {"name" : "Jack Sparrow", "Position": "Pirate", "salary": 30000}}
+
+def abort_if_employees_not_exist(name):
+    if name not in employees:
+        abort(404, message="Employee name does not exist")
 
 class IncreaseSalary(Resource):
-    def get(self):
-        return {"message": 'This URL will accept a PUT call with the following payload : '
-                           '{"value": "<value goes here>", "mode": "phone || name || amount", "replace_with": "--blank-- || --original--"}'}
+    def get(self, name):
+        abort_if_employees_not_exist(name)
+        return employees[name], 200
 
-    def post(self):
-        args = post_args.parse_args()
-        output_data = processing_data(args)
-        return output_data, 201
+    def put(self, name):
+        args = put_args.parse_args()
+        employees[name] = args
+        # output_data = processing_data(args)
+        return employees[name], 201
 
 
-api.add_resource(IncreaseSalary, "/")
+api.add_resource(IncreaseSalary, "/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
