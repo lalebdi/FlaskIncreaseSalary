22bf51504ea79e4f73d2dc354d7eb16a632388af
lalebdi <loai.alebdi@gmail.com>
2021-08-31 12:48:17 -0400

    pass tests

--- mock.py
+++ mock.py
@@ -4,73 +4,73 @@ BASE = "http://127.0.0.1:5000/"
 
 response = requests.get(BASE)
 print(response.json())
-#
-# input("Press Enter for next")
 
-# response = requests.get(BASE + "tony")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.get(BASE + "jack")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.get(BASE + "bruce")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.get(BASE + "steve")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.get(BASE + "unkown")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.put(BASE + "tony")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.put(BASE + "bruce")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.put(BASE + "jack")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.put(BASE + "steve")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-#
-# response = requests.put(BASE + "tony")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.put(BASE + "unkown")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.get(BASE + " ")
-# print(response.json())
-#
-# input("Press Enter for next")
-#
-# response = requests.put(BASE + " ")
-# print(response.json())
-#
+input("Press Enter for next")
+
+response = requests.get(BASE + "tony")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.get(BASE + "jack")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.get(BASE + "bruce")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.get(BASE + "steve")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.get(BASE + "unkown")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.put(BASE + "tony")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.put(BASE + "bruce")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.put(BASE + "jack")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.put(BASE + "steve")
+print(response.json())
+
+input("Press Enter for next")
+
+
+response = requests.put(BASE + "tony")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.put(BASE + "unkown")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.get(BASE + " ")
+print(response.json())
+
+input("Press Enter for next")
+
+response = requests.put(BASE + " ")
+print(response.json())
+
 input("Press Enter for next")
 
 response = requests.get(BASE + "")
