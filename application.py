40e5841a3084110e3055980f9b0f0be031d55c8f
lalebdi <loai.alebdi@gmail.com>
2021-08-30 21:36:06 -0400

    improve formatting

--- application.py
+++ application.py
@@ -1,16 +1,14 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, abort
 
 application = Flask(__name__)
 app = application
 api = Api(app)
 
-put_args = reqparse.RequestParser()
-put_args.add_argument("name", type=str, help="Name of employee is required")
-put_args.add_argument("salary", type=int, help="How much they make? it is required")
 
 employees = {"tony" : {"name" : "Tony Stark", "salary": 10000},
             "bruce": {"name" : "Bruce Wayne", "salary": 20000},
+            "steve": {"name" : "Steve Rogers", "salary": 20000.4},
             "jack": {"name" : "Jack Sparrow", "salary": 30000}}
 
 def abort_if_employees_not_exist(name):
@@ -22,6 +20,10 @@ def process_increase_salary(profile):
     initial_salary = employees[profile]["salary"]
     PERCENT_INCREASE = 1.05
     post_increase = initial_salary * PERCENT_INCREASE
+    if post_increase.is_integer():
+        post_increase = int(post_increase)
+    else:
+        post_increase = "{:.2f}".format(post_increase)
     employees[profile]["salary"] = post_increase
 
 
@@ -32,7 +34,6 @@ class IncreaseSalary(Resource):
         return employees[name], 200
 
     def put(self, name):
-        args = put_args.parse_args()
         process_increase_salary(name)
         return employees[name], 201
 
